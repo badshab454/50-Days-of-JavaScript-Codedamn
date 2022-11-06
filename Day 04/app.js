0bd3e6edd48be3ff1430c3da5721bb242de2f8bd
badshab454 <badshab454@gmail.com>
2022-11-06 23:14:17 +0600

    js loaded

--- Day 04/app.js
+++ Day 04/app.js
@@ -3,6 +3,10 @@ const time = '12:10AM';
 function convertTo24HrsFormat(time) {
     // write your solution here
 
+    if((Number.isInteger(parseInt(time.charAt(0)))) && time.charAt(1) === ':') {
+        time = time.substr(0,3)+'0'+time.substr(3)
+    }
+
     return
 }
 
