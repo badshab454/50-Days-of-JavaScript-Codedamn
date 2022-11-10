725e57487d3a1d95f337cd9f8516272dad47c998
badshab454 <badshab454@gmail.com>
2022-11-10 23:04:18 +0600

    Day 05 Solution

--- Day 05/app.js
+++ Day 05/app.js
@@ -2,8 +2,19 @@ const str = 'XeroX';
 
 function getTheGapX(str) {
     // write your solution here
+    const firstIndex
+    const firstIndex = str.indexOf('X');
 
-    return
+    if(firstIndex === -1) {
+        return -1
+    }
+
+    const lastIndex = str.lastIndexOf('X');
+
+    if (firstIndex === lastIndex) {
+        return 0;
+    }
+    return lastIndex - firstIndex;
 }
 
 console.log(`Gap between the X's: ${getTheGapX(str)}`)
